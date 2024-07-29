f07f3f774073897908cd82f6e24b3bc39393b607
krisluczka <krzysztofluczka@outlook.com>
2024-07-29 23:57:37 +0200

    http_get() linux version

--- scrapium.h/http_get.cpp
+++ scrapium.h/http_get.cpp
@@ -1,9 +1,25 @@
 #include "scrapium.h"
+
+#ifdef _WIN32
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+#elif defined(__linux__)
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h> 
+
+#endif
+
 namespace scrapium {
+
+#ifdef _WIN32
     std::string http_get( const std::string& host, const std::string& path ) {
         WSADATA wsaData;
         SOCKET ConnectSocket( INVALID_SOCKET );
@@ -14,14 +30,14 @@ namespace scrapium {
         int recvbuflen( 512 );
         char recvbuf[512];
 
-        // Initialize Winsock
+        // initializing winsock
         iResult = WSAStartup( MAKEWORD( 2, 2 ), &wsaData );
         if ( iResult != 0 ) {
             std::cerr << "WSAStartup failed: " << iResult << std::endl;
             return "";
         }
 
-        // Resolve the server address and port
+        // resolving the server and address port
         ZeroMemory( &hints, sizeof( hints ) );
         hints.ai_family = AF_INET;
         hints.ai_socktype = SOCK_STREAM;
@@ -34,7 +50,7 @@ namespace scrapium {
             return "";
         }
 
-        // Create a SOCKET for connecting to server
+        // creating a socket
         ConnectSocket = socket( result->ai_family, result->ai_socktype, result->ai_protocol );
         if ( ConnectSocket == INVALID_SOCKET ) {
             std::cerr << "socket failed: " << WSAGetLastError() << std::endl;
@@ -43,7 +59,7 @@ namespace scrapium {
             return "";
         }
 
-        // Connect to server
+        // connecting to a server
         iResult = connect( ConnectSocket, result->ai_addr, (int)result->ai_addrlen );
         if ( iResult == SOCKET_ERROR ) {
             closesocket( ConnectSocket );
@@ -58,7 +74,7 @@ namespace scrapium {
             return "";
         }
 
-        // Send an HTTP GET request
+        // HTTP get
         std::string request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";
         iResult = send( ConnectSocket, request.c_str(), (int)request.length(), 0 );
         if ( iResult == SOCKET_ERROR ) {
@@ -68,7 +84,7 @@ namespace scrapium {
             return "";
         }
 
-        // Receive the response
+        // receiving
         response.clear();
         do {
             iResult = recv( ConnectSocket, recvbuf, recvbuflen, 0 );
@@ -84,10 +100,72 @@ namespace scrapium {
             }
         } while ( iResult > 0 );
 
-        // Cleanup
+        // cleaning up
         closesocket( ConnectSocket );
         WSACleanup();
 
         return response;
     }
+#elif defined(__linux__)
+    std::string http_get( const std::string& host, const std::string& path ) {
+        int sockfd;
+        struct addrinfo hints, * res, * p;
+        std::string response;
+        int status;
+        char recvbuf[512];
+        ssize_t bytes_received;
+        const int recvbuflen = sizeof( recvbuf );
+
+        // resolving the server and address port
+        memset( &hints, 0, sizeof hints );
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+
+        if ( (status = getaddrinfo( host.c_str(), "80", &hints, &res )) != 0 ) {
+            std::cerr << "getaddrinfo error: " << gai_strerror( status ) << std::endl;
+            return "";
+        }
+
+        // creating a socket
+        sockfd = socket( res->ai_family, res->ai_socktype, res->ai_protocol );
+        if ( sockfd == -1 ) {
+            std::cerr << "socket creation failed: " << strerror( errno ) << std::endl;
+            freeaddrinfo( res );
+            return "";
+        }
+
+        // connecting to a server
+        if ( connect( sockfd, res->ai_addr, res->ai_addrlen ) == -1 ) {
+            std::cerr << "connection failed: " << strerror( errno ) << std::endl;
+            close( sockfd );
+            freeaddrinfo( res );
+            return "";
+        }
+
+        freeaddrinfo( res );
+
+        // HTTP get
+        std::string request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";
+        if ( send( sockfd, request.c_str(), request.length(), 0 ) == -1 ) {
+            std::cerr << "send failed: " << strerror( errno ) << std::endl;
+            close( sockfd );
+            return "";
+        }
+
+        // receiving
+        response.clear();
+        while ( (bytes_received = recv( sockfd, recvbuf, recvbuflen, 0 )) > 0 ) {
+            response.append( recvbuf, bytes_received );
+        }
+
+        if ( bytes_received == -1 ) {
+            std::cerr << "recv failed: " << strerror( errno ) << std::endl;
+        }
+
+        // cleaning up
+        close( sockfd );
+
+        return response;
+    }
+#endif
 }
