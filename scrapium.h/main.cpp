9196a8ffe8e31df4254fb7e7c13f9732da255cf1
krisluczka <krzysztofluczka@outlook.com>
2024-07-29 23:08:13 +0200

    Dodaj pliki projektów.

---
+++ scrapium.h/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "scrapium.h"
+
+int main() {
+    std::string host = "example.com";
+    std::string path = "/";
+    std::string response = getHTTPResponse( host, path );
+
+    std::cout << response;
+
+    return 0;
+}
